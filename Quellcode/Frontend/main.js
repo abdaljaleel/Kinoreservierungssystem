76774dcbae9ae83985bdf4dee5d278c3c522bfd9
I516994 <johannes.knoll@sap.com>
2020-09-07 10:05:15 +0200

    Added pictures at rectangles

--- Quellcode/Frontend/main.js
+++ Quellcode/Frontend/main.js
@@ -4,13 +4,19 @@ const table = document.getElementById("moviesOverview");
 
 var maxRow = 2;
 var maxCol = 5;
+var image = document.createElement("img");
+image.src = "JamesBond.png";
+image.className = "image";
 
 for (let row = 0; row < maxRow; row++) {
     var tableRow = document.createElement("tr");
     for (let col = 0; col < maxCol; col++) {
         var tableData = document.createElement("td");
         var div = document.createElement("div");
+        var divText = document.createElement("div");
         div.className = "moviePoster";
+        div.appendChild(image);
+        div.appendChild(divText);
         tableData.appendChild(div);
         tableRow.appendChild(tableData);
     }
