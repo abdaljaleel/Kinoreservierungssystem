4f9ad04feb6dcfe521c4c4f6c6d6d8fc0a9da54f
FinnHuelsbusch <f.huelsbusch@outlook.de>
2020-09-08 09:46:03 +0200

    added databse connection

--- Quellcode/Backend/Java/src/main/java/saue/kinoticketreservierungssystem/KinoticketreservierungssystemApplication.java
+++ Quellcode/Backend/Java/src/main/java/saue/kinoticketreservierungssystem/KinoticketreservierungssystemApplication.java
@@ -1,9 +1,11 @@
 package saue.kinoticketreservierungssystem;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class KinoticketreservierungssystemApplication {
 
     public static void main(String[] args) {
