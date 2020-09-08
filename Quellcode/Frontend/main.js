08687bde6644f79232dc05c1c645b4f623a384bd
Robin Baeurle <robin.baeurle@sap.com>
2020-09-08 17:52:34 +0200

    improved frontend

--- Quellcode/Frontend/main.js
+++ Quellcode/Frontend/main.js
@@ -20,8 +20,32 @@ for (let row = 0; row < maxRow; row++) {
         div.className = "moviePoster";
         div.appendChild(image);
         div.appendChild(text);
+        div.addEventListener("click", function () { displayMovieDetails(text.innerHTML); });
         table.appendChild(div);
     }
     // table.appendChild(tableRow);
+}
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+function displayMovieDetails(movie) {
+    modal.style.display = "block";
+    document.getElementById("modalMovieDetails").innerHTML = movie;
 }
 
