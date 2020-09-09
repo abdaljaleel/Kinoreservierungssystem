801bb5749b925cd86361de7349901d893bdb0f93
FinnHuelsbusch <f.huelsbusch@outlook.de>
2020-09-09 17:43:23 +0200

    Added new route movie with the id as parameter to retrive a single movie. Renamed movie route to movies. Changed pom to make a war export easier. Removed HATEOAS URLs from jsons.

--- Quellcode/Backend/Java/src/main/java/saue/kinoticketreservierungssystem/KinoticketreservierungssystemApplication.java
+++ Quellcode/Backend/Java/src/main/java/saue/kinoticketreservierungssystem/KinoticketreservierungssystemApplication.java
@@ -3,10 +3,13 @@ package saue.kinoticketreservierungssystem;
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.rest.RepositoryRestMvcAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = RepositoryRestMvcAutoConfiguration.class)
 @EnableEncryptableProperties
-public class KinoticketreservierungssystemApplication {
+public class KinoticketreservierungssystemApplication  {
 
     public static void main(String[] args) {
         SpringApplication.run(KinoticketreservierungssystemApplication.class, args);
