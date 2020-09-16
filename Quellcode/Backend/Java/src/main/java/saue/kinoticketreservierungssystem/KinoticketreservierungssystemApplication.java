9baa5670d93f875d7bb470ba415e6ec1f120bd7e
FinnHuelsbusch <f.huelsbusch@outlook.de>
2020-09-16 08:42:49 +0200

    added name to pom

--- Quellcode/Backend/Java/src/main/java/saue/kinoticketreservierungssystem/KinoticketreservierungssystemApplication.java
+++ Quellcode/Backend/Java/src/main/java/saue/kinoticketreservierungssystem/KinoticketreservierungssystemApplication.java
@@ -9,7 +9,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication//(exclude = RepositoryRestMvcAutoConfiguration.class)
 @EnableEncryptableProperties
-public class KinoticketreservierungssystemApplication  {
+public class KinoticketreservierungssystemApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(KinoticketreservierungssystemApplication.class, args);
