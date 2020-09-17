1397e2e6450518373cc6349ea7ed5b046e8a261d
I516994 <johannes.knoll@sap.com>
2020-09-17 09:14:14 +0200

    closing modal by clicking outside

--- Quellcode/Frontend/kino-app/src/MainModal/MainModal.js
+++ Quellcode/Frontend/kino-app/src/MainModal/MainModal.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import "./styles.less";
+import OutsideClickHandler from 'react-outside-click-handler';
 import { OverviewWindow } from '../OverviewWindow/OverviewWindow';
 import { ShowtimesWindow } from '../ShowtimesWindow/ShowtimesWindow';
 import { SeatsWindow } from '../SeatsWindow/SeatsWindow';
@@ -7,6 +8,7 @@ import { PaymentWindow } from '../PaymentWindow/PaymentWindow';
 
 const firstWindow = 0;
 const lastWindow = 3;
+const [hidden, setHidden] = useState(false);
 
 export class MainModal extends Component {
 
@@ -41,33 +43,37 @@ export class MainModal extends Component {
 
     render() {
         return (
-            <div className="modal-background">
-                <div className="modal">
+                <div className="modal-background">
+                    <OutsideClickHandler onOutsideClick={() => {
+                        setHidden(false);
+                    }}>
+                        <div className="modal"  onClick={() => setHidden(true)}></div>
+                    </OutsideClickHandler>
+                    {hidden && (
+                        <div>
+                            <div className="modal-header">
+                                <span className="close" onClick={e => this.closeModal()}>&times;</span>
+                                <h2 id="modal-heading" className="modal-heading">Modal Header</h2>
+                            </div>
 
-                    <div className="modal-header">
-                        <span className="close" onClick={e => this.closeModal()}>&times;</span>
-                        <h2 id="modal-heading" className="modal-heading">Modal Header</h2>
-                    </div>
+                            <div className="modal-content">
+                                {this.state.currentWindow === 0 && <OverviewWindow id="modal-window-overview"></OverviewWindow>}
+                                {this.state.currentWindow === 1 && <ShowtimesWindow id="modal-window-showtimes"></ShowtimesWindow>}
+                                {this.state.currentWindow === 2 && <SeatsWindow id="modal-window-seats" data-moviename="James Bond"></SeatsWindow>}
+                                {this.state.currentWindow === 3 && <PaymentWindow id="modal-window-payment"></PaymentWindow>}
+                            </div>
 
-                    <div className="modal-content">
-                        {this.state.currentWindow === 0 && <OverviewWindow id="modal-window-overview"></OverviewWindow>}
-                        {this.state.currentWindow === 1 && <ShowtimesWindow id="modal-window-showtimes"></ShowtimesWindow>}
-                        {this.state.currentWindow === 2 && <SeatsWindow id="modal-window-seats" data-moviename="James Bond"></SeatsWindow>}
-                        {this.state.currentWindow === 3 && <PaymentWindow id="modal-window-payment"></PaymentWindow>}
-                    </div>
-
-                    <div className="modal-footer">
-                        <div className="button-footer-back">
-                            <button id="btn-back" onClick={e => this.previousWindow()}>{this.state.currentWindow === firstWindow ? "Abbrechen" : "Zurück"}</button>
+                            <div className="modal-footer">
+                                <div className="button-footer-back">
+                                    <button id="btn-back" onClick={e => this.previousWindow()}>{this.state.currentWindow === firstWindow ? "Abbrechen" : "Zurück"}</button>
+                                </div>
+                                <div className="button-footer-continue">
+                                    <button id="btn-continue" onClick={e => this.nextWindow()}>{this.state.currentWindow === lastWindow ? "Bestätigen" : "Weiter"}</button>
+                                </div>
+                            </div>   
                         </div>
-                        <div className="button-footer-continue">
-                            <button id="btn-continue" onClick={e => this.nextWindow()}>{this.state.currentWindow === lastWindow ? "Bestätigen" : "Weiter"}</button>
-                        </div>
-                    </div>
-
+                    )}
                 </div>
-
-            </div>
         )
     }
 
--- Quellcode/Frontend/kino-app/src/OverviewWindow/OverviewWindow.js
+++ Quellcode/Frontend/kino-app/src/OverviewWindow/OverviewWindow.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import "./styles.less";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faWheelchair } from '@fortawesome/free-solid-svg-icons';
@@ -11,56 +11,56 @@ import { faStar } from '@fortawesome/free-solid-svg-icons';
 export class OverviewWindow extends Component {
 
     constructor(props) {
+
         super(props);
         this.state = {
-            
         };
     }
 
     render() {
         return (
-            <div className="modal-body" id="modal-body-ID">
-                <div className="movie-description-left">
-                    <p className="movie-description-headings">Beschreibung</p>
-                    <div className="movie-description">
-                        <p className="movie-description-content-div">Lorem ipsum dolor sit amet, consetetur
-                            sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
-                            ali gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-                        </p>
+                <div className="modal-body" id="modal-body-ID">
+                    <div className="movie-description-left">
+                        <p className="movie-description-headings">Beschreibung</p>
+                        <div className="movie-description">
+                            <p className="movie-description-content-div">Lorem ipsum dolor sit amet, consetetur
+                                sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                                ali gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                            </p>
+                        </div>
                     </div>
-                </div>
 
-                <div className="movie-description-center">
-                    <div className="movie-fsk">
-                        <FontAwesomeIcon icon={faUsersSlash}></FontAwesomeIcon>
-                        <a className="movie-description-content">12</a>
+                    <div className="movie-description-center">
+                        <div className="movie-fsk">
+                            <FontAwesomeIcon icon={faUsersSlash}></FontAwesomeIcon>
+                            <a className="movie-description-content">12</a>
+                        </div>
+                        <div className="movie-length">
+                            <FontAwesomeIcon icon={faStopwatch}></FontAwesomeIcon>
+                            <a className="movie-description-content">120</a>
+                        </div>
+                        <div className="movie-genre">
+                            <FontAwesomeIcon icon={faInfoCircle}></FontAwesomeIcon>
+                            <a className="movie-description-content">Action</a>
+                        </div>
+                        <div className="movie-actors">
+                            <FontAwesomeIcon icon={faUsers}></FontAwesomeIcon>
+                            <a className="movie-description-content">Daniel Craig</a>
+                            <p></p>
+                            <a className="movie-description-content">Daniel Craig</a>
+                        </div>
+                        <div className="movie-regisseur">
+                            <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
+                            <a className="movie-description-content">Sam Mendes</a>
+                        </div>
                     </div>
-                    <div className="movie-length">
-                        <FontAwesomeIcon icon={faStopwatch}></FontAwesomeIcon>
-                        <a className="movie-description-content">120</a>
-                    </div>
-                    <div className="movie-genre">
-                        <FontAwesomeIcon icon={faInfoCircle}></FontAwesomeIcon>
-                        <a className="movie-description-content">Action</a>
-                    </div>
-                    <div className="movie-actors">
-                        <FontAwesomeIcon icon={faUsers}></FontAwesomeIcon>
-                        <a className="movie-description-content">Daniel Craig</a>
-                        <p></p>
-                        <a className="movie-description-content">Daniel Craig</a>
-                    </div>
-                    <div className="movie-regisseur">
-                        <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
-                        <a className="movie-description-content">Sam Mendes</a>
-                    </div>
-                </div>
 
-                <div className="movie-description-right">
-                    <iframe src="https://www.youtube.com/embed/xZaZEQPSZd0" width="100%" height="300"
-                        style={{border:"none"}}></iframe>
-                </div>
+                    <div className="movie-description-right">
+                        <iframe src="https://www.youtube.com/embed/xZaZEQPSZd0" width="100%" height="300"
+                            style={{border:"none"}}></iframe>
+                    </div>
 
-            </div>
+                </div>
         )
     }
 
