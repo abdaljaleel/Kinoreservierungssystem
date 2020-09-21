9b8d771be705bb2703d42e4ca3ac6de421b4f8fc
Suliman Abu Ghaida <s.abughaida@outlook.de>
2020-09-21 18:32:45 +0200

    update fotter

--- Quellcode/Frontend/kino-app/src/navbar Components/Navigation/MainNavigation.js
+++ Quellcode/Frontend/kino-app/src/navbar Components/Navigation/MainNavigation.js
@@ -10,7 +10,7 @@ const MainNavigation = props => (
     <nav className="Main-Navigation__items">
     <ul>
     <li> <a href="#">Home</a> </li>
-          <li> <a href="#">Contact</a> </li>
+          <li> <a href="#contact">Contact</a> </li>
           <li> <a href="https://www.google.de/maps/place/Schmitthennerstra%C3%9Fe,+69124+Heidelberg/data=!4m2!3m1!1s0x4797c0b9876b62b7:0x94e37dcfab2eab09?sa=X&ved=2ahUKEwivyf6L3PXrAhWM2KQKHUEzCBkQ8gEwAHoECAQQAQ">Adress</a> </li>
           <li> <a href="#">Impressum</a> </li>
 
